5786a564c8005de2e6cb4f1bf1f391ee8bcd4671
Rani <rani.mimanshatech@gmail.com>
2022-11-05 05:55:49 +0530

    string

--- quicksort_Partition.py
+++ quicksort_Partition.py
@@ -1,13 +1,18 @@
 def quickSort(ar):
     mid,left,right=[],[],[]
     mid.append(ar[0])
+    ar[0]
+    
     for i in range(len(ar)):
         if (ar[i]<ar[0]):
             left.append(ar[i])
         if ar[i]>ar[0]:
             right.append(ar[i])
     print(left+mid+right)
-    # print(mid)
+    print(left+ar[0]+right)
+    print()
+    print("mid:",mid)
+    print("a[0]:",ar[0])
 
 arr=[5,7,4,3,8]
 quickSort(arr)
--- superReducedStr.py
+++ superReducedStr.py
@@ -1,15 +1,32 @@
-# def superReducedString(str):
-#     arr=[]
-#     for i in range(len(str)):
-#         print(str[i])
-#         if str[i]==None:
-#             continue
-#         if str[i]==str[i-1]:
-#             print("yes")
-#             arr.append(str[i])
-#             arr.append(str[i-1])
-#             arr.pop(i)
-#             # arr.pop(1)
-#             print(arr)
-# str="aabb"
-# superReducedString(str)
+def superReducedString(str):
+    arr=[]
+    count=0
+    res=[]
+    for i in range(len(str)):
+        arr.append(str[i])
+    n=len(arr)
+    for i in range(0,n):
+        dup=1
+        for j in range(i+1,n):
+            if arr[i]==None:
+                continue
+            if arr[i]==arr[j] and dup==1:
+                count+=1
+                dup+=1
+                # temp.append(arr[i])
+                # temp.append(arr[j])
+                arr[j]=None
+                arr[i]=None
+            else:
+                res.append(arr[i])
+    # print(arr)
+    # print(count)   
+    # print(temp)
+    # print(res)
+    result=''
+    for i in range(len(arr)):
+        if(arr[i]!=None):
+            result+=arr[i]
+    print(result)   
+str="I looveee yooouuu Mistii"
+superReducedString(str)
